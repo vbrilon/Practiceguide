850d6de8e3e7d96510fdea1b29a23d4983324c1e
Victor Brilon <victor@victorland.com>
2012-02-16 21:40:31 -0800

    Added wiki

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+admin:192ff8fcf3b2f87ef5f4a1a3d9b003e3:Victor:victor@victorland.com:admin,user
