5d43fd09cb5fd117910e2639c5511729f6d601a5
Victor Brilon <victor@victorland.com>
2012-03-10 01:00:24 -0800

    user app added

---
+++ guitarlog/user/views.py
@@ -0,0 +1,26 @@
+from user.forms import *
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response, get_object_or_404,redirect
+from django.template import RequestContext
+from django.contrib.auth import login, authenticate
+
+
+def register_page(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      user = User.objects.create_user(
+        username = form.cleaned_data['username'],
+        password = form.cleaned_data['password1'],
+        email = form.cleaned_data['email'],
+       )
+      user.save()
+      user2 = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+      login(request, user2)
+      return HttpResponseRedirect('/register/success/')
+  else:
+    form = RegistrationForm()
+  variables = RequestContext(request, {'form': form})
+  return render_to_response('registration/register.html',variables)
+    #return render_to_response('registration/register.html',RequestContext(request))
