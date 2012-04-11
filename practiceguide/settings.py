a290338f222dc0aafb82482b4e33e8a118f87dc0
Gabe Kneisley <gabekneisley@gmail.com>
2012-04-11 00:01:05 -0700

    not much

--- practiceguide/settings.py
+++ practiceguide/settings.py
@@ -119,9 +119,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-		'practice',
-		'user',
-		'django_gravatar',
+    'practice',
+    'user',
+    'django_gravatar',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
