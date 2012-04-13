b5b2edcfcd43f4c418c5689c3b25d71545969c31
Gabe Kneisley <gabekneisley@gmail.com>
2012-04-12 18:18:32 -0700

    Stuff not to keep

--- practiceguide/site_media/js/script.js
+++ practiceguide/site_media/js/script.js
@@ -1,4 +1,5 @@
 $("document").ready(function() {
+	/*
 	if( $("#signin_modal").length > 0 ) {
 		$("#signin_modal").modal({
 			keyboard: true
@@ -8,4 +9,5 @@ $("document").ready(function() {
 			$("#signin_modal").modal("show");
 		});
 	}
+	*/
 });
