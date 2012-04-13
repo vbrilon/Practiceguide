b4d789090628f16d5d425c913d60e2b3a4f4ff48
Gabe Kneisley <gabekneisley@gmail.com>
2012-04-12 18:56:06 -0700

    idr

--- practiceguide/site_media/js/script.js
+++ practiceguide/site_media/js/script.js
@@ -6,7 +6,22 @@ $("document").ready(function() {
 			$(href).modal('show');
 		} else {
 			$('<iframe class="modal" src="' + href + '"></iframe>').modal('show').appendTo('body');
-			
 		}
 	});
 });
+
+/*
+$("document").ready(function() {
+	$('*[data-modal]').click(function(e) {
+		e.preventDefault();
+		var href = $(e.target).attr('href');
+		if (href.indexOf('#') == 0) {
+			$(href).modal('show');
+		} else {
+			$.get(href, function(data) {
+				$('<div class="modal">' + data + '</div>').modal('show').appendTo('body');
+			});
+		}
+	});
+});
+*/
