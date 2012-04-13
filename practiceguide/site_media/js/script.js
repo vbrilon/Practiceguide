a2304d3e119d19978cb2e489cb1a7ea91c6141e3
Victor Brilon <victor@victorland.com>
2012-04-12 18:16:18 -0700

    modal login box

--- practiceguide/site_media/js/script.js
+++ practiceguide/site_media/js/script.js
@@ -1,3 +1,4 @@
+/*
 $("document").ready(function() {
 	if( $("#signin_modal").length > 0 ) {
 		$("#signin_modal").modal({
@@ -8,4 +9,19 @@ $("document").ready(function() {
 			$("#signin_modal").modal("show");
 		});
 	}
+});
+*/
+
+$("document").ready(function() {
+	$('*[data-modal]').click(function(e) {
+		e.preventDefault();
+		var href = $(e.target).attr('href');
+		if (href.indexOf('#') == 0) {
+			$(href).modal('show');
+		} else {
+			$.get(href, function(data) {
+				$('<div class="modal">' + data + '</div>').modal('show').appendTo('body');
+			});
+		}
+	});
 });
