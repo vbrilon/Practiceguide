394eca70f99babd499e4f818c596cc3b8e1229aa
Gabe Kneisley <gabekneisley@gmail.com>
2012-04-12 17:58:02 -0700

    More UI changes

--- practiceguide/site_media/js/homepage.js
+++ practiceguide/site_media/js/homepage.js
@@ -1,5 +1,5 @@
 $("document").ready(function() {
 	$("#enticement_button").click(function() {
 		alert("let's open a cool video on a modal.");
-	})
+	});
 });
--- practiceguide/site_media/js/script.js
+++ practiceguide/site_media/js/script.js
@@ -1,3 +1,11 @@
 $("document").ready(function() {
-	
+	if( $("#signin_modal").length > 0 ) {
+		$("#signin_modal").modal({
+			keyboard: true
+		});
+		
+		$("#signin_link").click(function( event ) {
+			$("#signin_modal").modal("show");
+		});
+	}
 });
